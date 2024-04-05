a12d58c2655b1d4e50d8836c0835a7d30be57bc4
Dharyl Carry S. Almora <37977733+dharylovey@users.noreply.github.com>
2024-04-05 22:06:25 +0800

    first commit

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/auth/login-form";
+
+const Login = () => {
+    return ( 
+        <div>
+            <LoginForm />
+        </div>
+     );
+}
+ 
+export default Login;
+
